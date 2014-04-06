adae75d3fb83a9168964a02a3d3e7cef2b04262a
Alexey Gromov <italianwind@gmail.com>
2014-04-07 00:03:22 +0400

    NEW Added ajax navigation

--- src/NS/FrontBundle/Resources/public/js/custom.js
+++ src/NS/FrontBundle/Resources/public/js/custom.js
@@ -1,15 +1,7 @@
 $(window).load(function () {
-    $('.flexslider').flexslider({
-        animation: "slide",
-        animationLoop: false,
-        slideshow: false
-    });
-
-    jQuery('.blockScroll, .scrollCont').jScrollPane({
-        showArrows:true,
-        verticalDragMinHeight: 29,
-        verticalDragMaxHeight: 29
-    });
+    navistudio.initSlider();
+    navistudio.initScroll();
+    navistudio.initNav();
 });
 
 (function($){
@@ -46,6 +38,92 @@ $(window).load(function () {
                     }
                 }
             );
+        },
+
+        /**
+         * Initializes slider
+         */
+        initSlider: function(){
+            $('.flexslider').flexslider({
+                animation: "slide",
+                animationLoop: false,
+                slideshow: false
+            });
+        },
+
+        /**
+         * Initializes scroll
+         */
+        initScroll: function(){
+            $('.blockScroll, .scrollCont').jScrollPane({
+                showArrows:true,
+                verticalDragMinHeight: 29,
+                verticalDragMaxHeight: 29
+            });
+        },
+
+        /**
+         * Initializes map
+         */
+        initMap: function(){
+            // map
+            var _m = $('.ns-map');
+            if (_m.length) {
+                var mapOptions = {
+                    center: new google.maps.LatLng(55.777306, 37.643594),
+                    zoom: 17,
+                    scrollwheel: false
+                };
+                var map = new google.maps.Map(_m.get(0), mapOptions);
+
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(55.777306, 37.643594),
+                    map: map,
+                    title:""
+                });
+            }
+        },
+
+        /**
+         * Initializes ajax navigation
+         */
+        initNav: function(){
+            if (!History.enabled) return;
+
+            History.Adapter.bind(window,'statechange', function() {
+                var res = History.getState().data.res;
+                var html = $(res);
+
+                // replacing content
+                $('.boxMain').html(html.find('.boxMain').html());
+                $('.mainNav').html(html.find('.mainNav').html());
+
+                // fixing page class
+                $('body > div').get(0).className = html.find('.repeat-x').parent().attr('class');
+
+                // removing scroll
+                if (html.find('.news, .clients').length) {
+                    $('.blockCont').removeClass('scrollCont').css({'background':'none'});
+                }
+
+                // reinitializing
+                navistudio.initSlider();
+                navistudio.initScroll();
+                navistudio.initMap();
+            });
+
+            //init
+//            initState = $('body').data('init');
+//            History.replaceState(initState, initState.data.title, location.pathname + location.search);
+
+
+            $('a').live('click', function(){
+                var href = $(this).attr('href');
+                $.get(href, function(res){
+                    History.pushState({res:res}, '', href);
+                });
+                return false;
+            });
         }
     };
 })(jQuery);
